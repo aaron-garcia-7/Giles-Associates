4c71debb94180b8247bcc04dec86059952b039bf
Aaron Garcia <aaronegarcia1@gmail.com>
2023-01-14 15:57:19 -0700

    update svg icon file name to lowercase

--- data/perks.js
+++ data/perks.js
@@ -1,12 +1,3 @@
-import {
-  faCompass,
-  faClock,
-  faSackDollar,
-  faCouch,
-  faUser,
-  faLightbulb,
-} from "@fortawesome/free-solid-svg-icons";
-
 import compass from "../images/icons/compass.svg";
 import clock from "../images/icons/clock.svg";
 import dollar from "../images/icons/dollar.svg";
@@ -19,43 +10,31 @@ const perks = [
     title1: "Be your",
     title2: "own boss",
     icon: compass,
-    // icon: faCompass,
-    // description: `We're here to support you from the start.`,
   },
   {
     title1: "Set your own",
     title2: "schedule and fees",
     icon: clock,
-    // icon: faClock,
-    // description: `We know that it's YOUR business.`,
   },
   {
     title1: "Keep 100% of",
     title2: "what you make",
     icon: dollar,
-    // icon: faSackDollar,
-    // description: `Custom designed suites that you're going to love!`,
   },
   {
     title1: "Private, furnished",
     title2: "offices to lease",
     icon: couch,
-    // icon: faCouch,
-    // description: `Work how you want, when you want.`,
   },
   {
     title1: "Referrals to build",
     title2: "your practice",
     icon: user,
-    // icon: faUser,
-    // description: `Work how you want, when you want.`,
   },
   {
     title1: "Opportunities",
     title2: "for mentorship",
     icon: bulb,
-    // icon: faLightbulb,
-    // description: `Work how you want, when you want.`,
   },
 ];
 
